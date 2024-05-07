3334744c63950e2eac5b7d05e36c9a29a05fef8e
Danylo Holets <90461399+dan1rock@users.noreply.github.com>
2024-05-08 01:50:22 +0300

    Modified token auth to work with auth0, added user creation and token refreshment

--- token_auth/index.js
+++ token_auth/index.js
@@ -6,13 +6,17 @@ const path = require('path');
 const port = 3000;
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const SESSION_KEY = 'Authorization';
-const JWT_SECRET_KEY = 'SECRET';
+
+const AUTH0_DOMAIN = 'danylo.eu.auth0.com';
+const AUTH0_CLIENT_ID = 'RhGEaZ2CjYpuHfAR7gz3BxF7Fk59Bkbh';
+const AUTH0_CLIENT_SECRET = 'eui8xIPZjM8Edq3RbqhX6pWxgGfZfuzj9xDm4yKX9-LsOjnqh7CWbJGe6Z8n4J_T';
 
 class Session {
     #sessions = {}
@@ -22,7 +26,7 @@ class Session {
             this.#sessions = fs.readFileSync('./sessions.json', 'utf8');
             this.#sessions = JSON.parse(this.#sessions.trim());
 
-            console.log(this.#sessions);
+            //console.log(this.#sessions);
         } catch(e) {
             this.#sessions = {};
         }
@@ -62,48 +66,56 @@ const sessions = new Session();
 
 app.use((req, res, next) => {
     let currentSession = {};
-    let jwtToken = req.get(SESSION_KEY);
+    let sessionId = req.get(SESSION_KEY);
 
-    if (jwtToken) {
-        try {
-            jwt.verify(jwtToken, JWT_SECRET_KEY);
-            currentSession = sessions.get(jwtToken);
-            if (!currentSession) {
-                currentSession = {};
-                jwtToken = sessions.init(res);
-            }
-        } catch (err) {
-            jwtToken = sessions.init(res);
+    if (sessionId) {
+        currentSession = sessions.get(sessionId);
+        if (!currentSession) {
+            currentSession = {};
+            sessionId = sessions.init(res);
         }
     } else {
-        jwtToken = sessions.init(res);
+        sessionId = sessions.init(res);
     }
 
     req.session = currentSession;
-    req.sessionId = jwtToken;
+    req.sessionId = sessionId;
 
     onFinished(req, () => {
         const currentSession = req.session;
-        const jwtToken = req.sessionId;
-        sessions.set(jwtToken, currentSession);
+        const sessionId = req.sessionId;
+        sessions.set(sessionId, currentSession);
     });
 
-    next();
+    if (req.session.access_token && req.session.refresh_token) {
+        const accessToken = jwt.decode(req.session.access_token);
+        const currentTime = Math.floor(Date.now() / 1000);
+
+        if (accessToken.exp < currentTime) {
+            axios.post('http://localhost:3000/api/refreshToken', {
+                refresh_token: req.session.refresh_token
+            })
+                .then(response => {
+                    req.session.access_token = response.data.access_token;
+                    req.session.refresh_token = response.data.refresh_token;
+                    next();
+                })
+                .catch(error => {
+                    console.error('Token refresh failed:', error.response ? error.response.data : error.message);
+                    res.status(401).send('Token refresh failed');
+                });
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
 })
 
-const authenticateJWT = (req, res, next) => {
-    const token = req.headers.authorization;
-
-    jwt.verify(token, JWT_SECRET_KEY, (err, decoded) => {
-        req.user = decoded;
-        next();
-    });
-};
-
-app.get('/', authenticateJWT, (req, res) => {
-    if (req.user) {
+app.get('/', (req, res) => {
+    if (req.session.username) {
         return res.json({
-            username: req.user.username,
+            username: req.session.username,
             logout: 'http://localhost:3000/logout'
         })
     }
@@ -115,38 +127,74 @@ app.get('/logout', (req, res) => {
     res.redirect('/');
 });
 
-const users = [
-    {
-        login: 'Login',
-        password: 'Password',
-        username: 'Username',
-    },
-    {
-        login: 'Login1',
-        password: 'Password1',
-        username: 'Username1',
-    }
-]
+app.post('/api/login', async (req, res) => {
+    const {login, password} = req.body;
 
-app.post('/api/login', (req, res) => {
-    const { login, password } = req.body;
+    try {
+        const response = await axios.post(`https://${AUTH0_DOMAIN}/oauth/token`, {
+            grant_type: 'password',
+            username: login,
+            password: password,
+            client_id: AUTH0_CLIENT_ID,
+            client_secret: AUTH0_CLIENT_SECRET,
+            scope: 'offline_access'
+        });
 
-    const user = users.find((user) => {
-        if (user.login == login && user.password == password) {
-            return true;
+        const { access_token, refresh_token } = response.data;
+        const userId = jwt.decode(access_token).sub;
+
+        req.session.username = userId;
+        req.session.access_token = access_token;
+        req.session.refresh_token = refresh_token;
+
+        res.json({ token: req.sessionId });
+    } catch (error) {
+        if (error.response && error.response.data) {
+            console.error('Login failed:', error.response.data);
+        } else {
+            console.error('Login failed:', error.message);
         }
-        return false
-    });
-
-    if (user) {
-        req.session.username = user.username;
-        req.session.login = user.login;
-        const jwtToken = jwt.sign({ username: user.username }, JWT_SECRET_KEY);
-
-        res.json({ username: user.username, token: jwtToken });
+        res.status(401).send('Login failed');
     }
+});
 
-    res.status(401).send();
+app.post('/api/createUser', async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        const response = await axios.post(`https://${AUTH0_DOMAIN}/dbconnections/signup`, {
+            email: email,
+            password: password,
+            client_id: AUTH0_CLIENT_ID,
+            connection: 'Username-Password-Authentication'
+        });
+
+        res.status(201).send('User created successfully');
+    } catch (error) {
+        console.error('User creation failed:', error.response ? error.response.data : error.message);
+        res.status(500).send('User creation failed');
+    }
+});
+
+app.post('/api/refreshToken', async (req, res) => {
+    const refreshToken = req.body.refresh_token;
+
+    try {
+        const response = await axios.post(`https://${AUTH0_DOMAIN}/oauth/token`, {
+            grant_type: 'refresh_token',
+            client_id: AUTH0_CLIENT_ID,
+            client_secret: AUTH0_CLIENT_SECRET,
+            refresh_token: refreshToken
+        });
+
+        const { access_token, refresh_token } = response.data;
+
+        console.log(refresh_token);
+        res.json({ access_token, refresh_token });
+    } catch (error) {
+        console.error('Token refresh failed:', error.response ? error.response.data : error.message);
+        res.status(401).send('Token refresh failed');
+    }
 });
 
 app.listen(port, () => {
